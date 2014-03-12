459459cfc20864ba9c69bf40b784189b6a9c7e63
Dave Airlie <airlied@redhat.com>
2014-03-12 13:25:15 +0100

    virtgpu: set pci bus id
    So X will autoload modesetting.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/virtio/virtgpu_drm_bus.c
+++ drivers/gpu/drm/virtio/virtgpu_drm_bus.c
@@ -1,4 +1,5 @@
 #include <drm/drmP.h>
+#include <linux/pci.h>
 
 static int drm_virtio_get_irq(struct drm_device *dev)
 {
@@ -11,8 +12,38 @@ static const char *drm_virtio_get_name(struct drm_device *dev)
 }
 
 static int drm_virtio_set_busid(struct drm_device *dev,
-			       struct drm_master *master)
+				struct drm_master *master)
 {
+	struct pci_dev *pdev = dev->pdev;
+	int len;
+
+	if (pdev) {
+		/* mimic drm_pci_set_busid() */
+		master->unique_len = 40;
+		master->unique_size = master->unique_len;
+		master->unique = kmalloc(master->unique_size, GFP_KERNEL);
+		if (master->unique == NULL)
+			return -ENOMEM;
+		len = snprintf(master->unique, master->unique_len,
+			       "pci:%04x:%02x:%02x.%d",
+			       pci_domain_nr(pdev->bus),
+			       pdev->bus->number,
+			       PCI_SLOT(pdev->devfn),
+			       PCI_FUNC(pdev->devfn));
+		if (len >= master->unique_len) {
+			DRM_ERROR("buffer overflow");
+			return -EINVAL;
+		}
+		master->unique_len = len;
+
+		dev->devname =
+			kmalloc(strlen("virtio") +
+				master->unique_len + 2, GFP_KERNEL);
+		if (dev->devname == NULL)
+			return -ENOMEM;
+		sprintf(dev->devname, "%s@%s", "virtio",
+			master->unique);
+	}
 	return 0;
 }
 
